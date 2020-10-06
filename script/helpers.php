46cfb6daac6ae0fa9a5eae651d74ec9e353852de
cbl <lennart.carbe@gmail.com>
2020-10-06 16:10:52 +0200

    path helper

---
+++ script/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (! function_exists('litstack_lang_path')) {
+    /**
+     * Get path to language files.
+     *
+     * @return string
+     */
+    function litstack_lang_path()
+    {
+        return __DIR__.'/../src';
+    }
+}
