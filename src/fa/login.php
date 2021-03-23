cd66fa3a93fa1a2f352d47fa45c2be443378faf7
Ali Ghasemzadeh <it.ghasemzadeh@gmail.com>
2021-03-23 11:49:02 +0430

    Adding Farsi Language.

---
+++ src/fa/login.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the translations for the login page.
+    |
+    */
+
+    'login'             => 'ورود',
+    'do_login'          => 'ورود',
+    'email_or_username' => 'نام کاربری یا ایمیل',
+    'remember_me'       => 'یادآوری',
+    'forgot_password'   => 'فراموشی رمز عبور',
+    'failed'            => 'اطلاعات وارد شده اشتباه است.',
+];
