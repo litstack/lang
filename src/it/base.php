cbe632bdfa3b8d84ad5d34becb33c1dbda588aa6
Luca Calcaterra <calcaterra.luca@gmail.com>
2021-04-17 21:02:15 +0200

    italian language - initial contribution

---
+++ src/it/base.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Base translations are words or word combinations that can occur frequently
+    | and do not specifically belong to any part of the litstack application.
+    |
+    */
+
+    'no_item_found'    => 'Nessun :item trovato',
+    'no_item_selected' => 'Nessun :item selezionato',
+    'item_delete'      => 'elimina :item',
+    'item_deleted'     => ':item eliminato',
+    'item_enter'       => 'inserisci :item',
+    'item_create'      => 'crea :item',
+    'item_remove'      => 'rimuovi :item',
+    'item_select'      => 'seleziona :item',
+    'item_order'       => 'ordina :item',
+    'item_ordered'     => ':item ordinato',
+    'item_search'      => 'cerca :item',
+    'item_add'         => 'aggiungi :item',
+    'item_added'       => ':item aggiunto',
+    'item_assign'      => 'assegna :item',
+    'item_settings'    => 'impostazioni :item',
+    'item_item'        => 'elemento :item',
+    'user_hello'       => 'Ciao :user',
+    'profile'          => 'profilo',
+    'settings'         => 'impostazioni',
+    'password'         => 'password',
+    'drag_and_drop'    => 'Drag and Drop',
+    'name'             => 'nome',
+    'user'             => 'utente',
+    'users'            => 'utenti',
+    'email'            => 'e-mail',
+    'email_address'    => 'indirizzo e-mail',
+    'language'         => 'Lingua',
+    'system_language'  => 'Lingua di sistema',
+    'general'          => 'generale',
+    'security'         => 'sicurezza',
+    'device'           => 'dispositivo',
+    'logout'           => 'esci',
+    'change_order'     => 'cambia ordine',
+    'login'            => 'login',
+    'close'            => 'chiudi',
+    'save'             => 'salva',
+    'saved'            => 'salvato',
+    'done'             => 'fatto',
+    'undo_changes'     => 'Annulla modifiche',
+    'delete'           => 'cancella',
+    'image'            => 'immagine',
+    'crop'             => 'ritaglia',
+    'search'           => 'cerca',
+    'first_name'       => 'Nome',
+    'last_name'        => 'Cognome',
+    'username'         => 'nome utente',
+    'cancel'           => 'annulla',
+    'location'         => 'posizione',
+    'action'           => 'azione|azioni',
+    'role'             => 'ruolo',
+    'roles'            => 'ruoli',
+    'read'             => 'leggi',
+    'update'           => 'aggiorna',
+    'sort'             => 'ordina',
+    'create'           => 'create',
+    'delete'           => 'delete',
+    'permissions'      => 'permessi',
+    'group'            => 'gruppo',
+    'unauthorized'     => 'Non autorizzato!',
+    'page'             => 'pagina',
+    'all'              => 'tutto',
+    'pages'            => 'pagine',
+    'hello'            => 'ciao',
+    'filter'           => 'filtra',
+    'toggle_all'       => 'attiva tutto',
+    'messages'         => [
+        'order_changed' => 'Ordine modificato correttamente',
+        'are_you_sure'  => 'Sei sicuro? Questa operazione non può essere annullata.',
+    ],
+];
