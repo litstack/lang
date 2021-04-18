ebad2086375736ab020df20b9424eca1ddadc619
afatmustafa <mustafa@afat.me>
2021-04-18 05:05:01 +0300

    Add Litstack Turkish language

---
+++ src/tr/login.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the translations for the login page.
+    |
+    */
+
+    'login'             => 'Giriş yap',
+    'do_login'          => 'Giriş yap',
+    'email_or_username' => 'E-posta yada kullanıcı adı',
+    'remember_me'       => 'Beni Hatırla',
+    'forgot_password'   => 'şifremi unuttum',
+    'failed'            => 'Geçersiz kimlik bilgileri',
+];
---
+++ src/tr/profile.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profile Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines translate all parts of the profile settings
+    | page.
+    |
+    */
+
+    'new_password'         => 'Yeni Şifre',
+    'new_password_confirm' => 'Yeni Şifreni Doğrula',
+    'current_password'     => 'Şimdiki Şidre',
+    'change_password'      => 'Şifreyi Değiştir',
+    'messages'             => [
+        'language' => 'Bu, yönetim arayüzünü gördüğünüz dildir.',
+    ],
+];
