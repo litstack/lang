04396ab9b4ae5b0cc5d0654429d9dd6371723ec2
Jackson <lordjackson@gmail.com>
2021-11-19 13:37:38 -0300

    add language pt-BR

---
+++ src/pt-BR/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset'     => 'Sua senha foi redefinida!',
+    'sent'      => 'Nós enviamos um link de redefinição de senha para seu e-mail!',
+    'throttled' => 'Por favor, aguarde um momento e tentar novamente .',
+    'token'     => 'Este token de redefinição de senha é inválido.',
+    'user'      => "Não podemos encontrar um usuário com esse endereço de e-mail.",
+    'buttons'   => [
+        'send_reset' => 'Enviar link de redefinição de senha',
+        'reset'      => 'Resetar senha',
+        'resend'     => 'Enviar novamente',
+    ],
+];
---
+++ src/pt-BR/profile.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profile Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines translate all parts of the profile settings
+    | page.
+    |
+    */
+
+    'new_password'         => 'Nova Senha',
+    'new_password_confirm' => 'Confirmar Nova SenhaConfirm New Password',
+    'current_password'     => 'Senha atual',
+    'change_password'      => 'Mudar senha',
+    'messages'             => [
+        'language' => 'Esta é a linguagem em que você vê a interface de administração.',
+    ],
+];
