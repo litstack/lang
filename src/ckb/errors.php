882b189d98b9da54512cf4c84d65df68775c3266
Swara <swara09@gmail.com>
2022-03-27 21:02:53 +0200

    Create errors.php

---
+++ src/ckb/errors.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Error Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for general error messages like
+    | http errors.
+    |
+    */
+
+    'not_found'         => 'نەدۆزرایەوە.',
+    'no_permission'     => 'ئەم کردارە ڕێگەپێنەدراوە!',
+    'server_error'      => 'هەڵەی ڕاژە.',
+    'too_many_requests' => 'داواکاری زۆر.',
