d023fff4672aa23fc7fe8879f4dc50635152c2da
Lars Krause <35800965+larshanskrause@users.noreply.github.com>
2022-05-20 16:54:10 +0200

    Update profile.php
    Added english translations for change email

--- src/en/profile.php
+++ src/en/profile.php
@@ -12,11 +12,13 @@ return [
     |
     */
 
-    'new_password'         => 'New Password',
-    'new_password_confirm' => 'Confirm New Password',
-    'current_password'     => 'Current Password',
-    'change_password'      => 'Change Password',
-    'messages'             => [
+    'new_password'          => 'New Password',
+    'new_password_confirm'  => 'Confirm New Password',
+    'current_password'      => 'Current Password',
+    'change_password'       => 'Change Password',
+    'change_email'          => 'Change E-Mail',
+    'password_confirm_hint' => 'Confirm with current password.',
+    'messages'              => [
         'language' => 'This is the language in which you see the administration interface.',
     ],
 ];
