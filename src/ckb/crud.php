9d49d8588e33da23a597a793f56fd63ef367b0bb
Swara <swara09@gmail.com>
2022-06-19 00:12:37 +0200

    Update crud.php

--- src/ckb/crud.php
+++ src/ckb/crud.php
@@ -19,15 +19,15 @@ return [
     'n_items_selected' => ':count بڕگە هەڵبژێردراوە | :count بڕگە هەڵبژێردراوە',
 
     'messages' => [
-        'not_created'   => ':model has to be created in order to add <i>:relation</i>.',
-        'deleted_items' => 'Deleted :count item.|Deleted :count items.',
+        'not_created'   => ':model دەبێت دروست بکرێت بۆ ئەوەی زیاد بکرێت <i>:relation</i>.',
+        'deleted_items' => ':count دانە سڕایەوە.|:count دانە سڕایەوە.',
     ],
 
     'fields' => [
         'block' => [
             'expand'       => 'فراوانکردن',
             'expand_all'   => 'فراوانکردنی هەموو',
-            'collapse_all' => 'collapse all',
+            'collapse_all' => 'نیشاندانی هەموو',
             'messages'     => [
                 'new_block' => 'زیادکردنی :type بلۆکی نوێ',
 
@@ -38,33 +38,33 @@ return [
             'unlink'   => 'پەیوەستنەکردنی پەیوەندیدار',
             'edit'     => 'دەستکاری پەیوەندیدار',
             'messages' => [
-                'relation_linked'   => '{relation} successfully linked.',
-                'relation_unlinked' => 'Relation unlinked.',
-                'confirm_unlink'    => 'Please confirm that you wish to unlink the item.',
-                'max_items_reached' => 'A maximum of :count items can be selected.',
+                'relation_linked'   => '{relation} بەسەرکەوتوویی پەیوەستکرا.',
+                'relation_unlinked' => 'پەیوەندی پچڕێندرا،',
+                'confirm_unlink'    => 'تکایە پشتڕاستی بکەرەوە کە دەتەوێت پەیوەندییەکە بپچڕێنیت.',
+                'max_items_reached' => 'زۆرترین... :count دانە دەتوانرێت هەڵبژێردرێت.',
             ],
         ],
         'wysiwyg' => [
-            'new_window' => 'open in new window',
+            'new_window' => 'کردنەوە لە پەنجەرەیەکی نوێدا',
         ],
         'list' => [
             'messages' => [
-                'max_depth'           => 'The list can be nested a maximum of :count levels.',
-                'confirm_delete'      => 'Should :item item really be deleted?',
-                'confirm_delete_info' => 'If you remove this item, you also remove all child items below it.',
+                'max_depth'           => 'دەتوانرێت لیستەکە زۆرترین هێلانە بکرێت :count ئاست.',
+                'confirm_delete'      => 'دەبێت :item بەڕاستی بسڕدرێتەوە?',
+                'confirm_delete_info' => 'ئەگەر ئەم بابەتە بسڕیتەوە، هەروەها هەموو پەیوەستکراوەکانی خوارەوەیش دەسڕدرێنەوە.',
             ],
         ],
         'media' => [
             'messages' => [
-                'image_uploaded'   => 'File successfully uploaded.',
-                'image_alt_hint'   => 'Alternative text (text that describes this medium) improves accessibility and user experience for people who cannot see images on Web pages, including users who use screen reader applications or have low bandwidth connections.',
-                'image_title_hint' => 'The title appears in the browser as a tooltip, if you move the mouse pointer over the image.',
+                'image_uploaded'   => 'پەڕگەکە بە سەرکەوتوویی بارکرا.',
+                'image_alt_hint'   => 'دەقی جێگرەوە (دەقێک کە باس لەم ڕەنگاڵەیە دەکات) دەستڕاگەیشتن و ئەزموونی بەکارهێنەر باشتر دەکات بۆ ئەو کەسانەی کە ناتوانن وێنە لە لاپەڕەکانی وێبدا ببینن، لەوانە ئەو بەکارهێنەرانەی کە بەرنامەکانی خوێنەری شاشە بەکاردەهێنن یان پەیوەندی باندبه‌رینی کەمیان هەیە.',
+                'image_title_hint' => 'ناونیشانەکە لە وێبگەڕەکەدا وەک ئامرازێک دەردەکەوێت، ئەگەر ئاماژەدەری ماوسەکە بەسەر وێنەکەدا بجوڵێنیت.',
             ],
         ],
     ],
     'meta' => [
-        'title_hint'       => 'Easily understandable meaningful sentence. Gives an idea what the page content is about. Maximum :width wide.',
-        'keywords_hint'    => 'The most important keywords of the page content. Single (few) words separated by commas.',
-        'description_hint' => 'Short but meaningful summary of the page. Contains the most important keywords of the page content.',
+        'title_hint'       => 'ڕستەیەکی مانادار بە ئاسانی تێدەگات. بیرۆکەیەک دەدات کە ناوەڕۆکی لاپەڕەکە باسی چی دەکات. زۆرترین :width پانی.',
+        'keywords_hint'    => 'گرنگترین وشە سەرەکییەکانی ناوەڕۆکی لاپەڕەکە. تاکە (چەند) وشەیەک کە بە کۆما جیا دەکرێنەوە.',
+        'description_hint' => 'پوختەیەکی کورت بەڵام مانادار لە لاپەڕەکە. گرنگترین وشە سەرەکییەکانی ناوەڕۆکی لاپەڕەکە لەخۆدەگرێت.',
     ],
 ];
